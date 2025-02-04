f0c7419277b6906700872cb66a2f9c102ad7b841
02Alexis <m_chael1012@hotmail.com>
2025-02-04 11:35:22 -0500

    etiquetas y botones

--- interfaz_grafica.py
+++ interfaz_grafica.py
@@ -4,4 +4,21 @@ window = tk.Tk()
 window.title("Interfaz Grafica")
 window.geometry("400x300")
 
+tag = tk.Label(window, text="Hola")
+tag.pack() # ---- posiciona los widgets dentro de la ventana
+
+# ---- funcion para cambiar el color de la etiqueta
+def change_color():
+    tag.config(fg="red")
+
+# ---- funcion para cambiar el color de la etiqueta
+def change_text():
+    tag.config(text="Adios Interfaz Grafica")
+
+button = tk.Button(window, text="Cambiar Texto", command=change_color)
+button.pack()
+
+button_bye = tk.Button(window, text="Adios Interfaz Grafica", command=change_text)
+button_bye.pack()
+
 window.mainloop() #------ mantiene la ventana abierta
